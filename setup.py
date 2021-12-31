2a7e82bc21946f3b26073d859ce1e77633875631
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-12-31 14:14:01 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import setuptools
-from setuptools import setup,find_packages
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-setuptools.setup(
-     name='abirvi',  
-     version='0.3',
-     scripts=['abir'] ,
-     author="ABIR HOSSAIN",
-     author_email="abirhossain200019@gmail.com",
-     description="this is a test file",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/ABIRHOSSAIN10/test",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 2.7",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
 import pathlib
 from setuptools import setup, find_packages
 
